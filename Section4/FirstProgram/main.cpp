5137f9a7e3fb510612e399e1721733fc7d1b83b2
BFB10\frank <frank@d-fect.com>
2018-05-07 16:22:39 +0200

    Section 4 added - Only one project of value for the course, Section_4_challenge

---
+++ Section4/FirstProgram/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() 
+{
+    int favourite_number = 0;
+    
+    std::cout << "Please enter your favourite number between 1 and 100: ";
+    std::cin >> favourite_number;
+    std::cout << "Amazing!! That's my favourite number too!" << std::endl;
+    std::cout << "No really!!, " << favourite_number << " is my favourite number!" << std::endl;
+    
+    return 0;
+}
---
+++ Section4/Project2/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello Project 2!" << std::endl;
+    
+    return 0;
+}
