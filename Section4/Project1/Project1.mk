cde764b3fe90f6c79fedadf2a18fa67bcab52509
BFB10\frank <frank@d-fect.com>
2018-07-15 15:51:00 +0200

    Section 4: Getting started

--- Section4/Project1/Project1.mk
+++ Section4/Project1/Project1.mk
@@ -5,18 +5,18 @@
 ## Debug
 ProjectName            :=Project1
 ConfigurationName      :=Debug
-WorkspacePath          :=C:/Programming/BeginningCpp/Section4
-ProjectPath            :=C:/Programming/BeginningCpp/Section4/Project1
+WorkspacePath          :=D:/UnrealCourse/Repos/BeginningCpp/Section4
+ProjectPath            :=D:/UnrealCourse/Repos/BeginningCpp/Section4/Project1
 IntermediateDirectory  :=./Debug
 OutDir                 := $(IntermediateDirectory)
 CurrentFileName        :=
 CurrentFilePath        :=
 CurrentFileFullPath    :=
 User                   :=frank
-Date                   :=12/06/2018
-CodeLitePath           :="C:/Program Files/CodeLite"
-LinkerName             :=C:/MinGW/bin/g++.exe
-SharedObjectLinkerName :=C:/MinGW/bin/g++.exe -shared -fPIC
+Date                   :=15/07/2018
+CodeLitePath           :="D:/Program Files/CodeLite"
+LinkerName             :=D:/MinGW/bin/g++.exe
+SharedObjectLinkerName :=D:/MinGW/bin/g++.exe -shared -fPIC
 ObjectSuffix           :=.o
 DependSuffix           :=.o.d
 PreprocessSuffix       :=.i
@@ -36,7 +36,7 @@ ObjectsFileList        :="Project1.txt"
 PCHCompileFlags        :=
 MakeDirCommand         :=makedir
 RcCmpOptions           := 
-RcCompilerName         :=C:/MinGW/bin/windres.exe
+RcCompilerName         :=D:/MinGW/bin/windres.exe
 LinkOptions            :=  
 IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
 IncludePCH             := 
@@ -49,19 +49,19 @@ LibPath                := $(LibraryPathSwitch).
 ## Common variables
 ## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
 ##
-AR       := C:/MinGW/bin/ar.exe rcu
-CXX      := C:/MinGW/bin/g++.exe
-CC       := C:/MinGW/bin/gcc.exe
+AR       := D:/MinGW/bin/ar.exe rcu
+CXX      := D:/MinGW/bin/g++.exe
+CC       := D:/MinGW/bin/gcc.exe
 CXXFLAGS := -std=c++14 -Wall -g -O0 -Wall $(Preprocessors)
 CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
 ASFLAGS  := 
-AS       := C:/MinGW/bin/as.exe
+AS       := D:/MinGW/bin/as.exe
 
 
 ##
 ## User defined environment variables
 ##
-CodeLiteDir:=C:\Program Files\CodeLite
+CodeLiteDir:=D:\Program Files\CodeLite
 Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
 
 
@@ -94,7 +94,7 @@ PreBuild:
 ## Objects
 ##
 $(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp $(IntermediateDirectory)/main.cpp$(DependSuffix)
-	$(CXX) $(IncludePCH) $(SourceSwitch) "C:/Programming/BeginningCpp/Section4/Project1/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/UnrealCourse/Repos/BeginningCpp/Section4/Project1/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
 $(IntermediateDirectory)/main.cpp$(DependSuffix): main.cpp
 	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/main.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/main.cpp$(DependSuffix) -MM main.cpp
 
--- Section4/Section_4_Challenge/main.cpp
+++ Section4/Section_4_Challenge/main.cpp
@@ -6,6 +6,7 @@ int main()
     
     std::cout << "Please enter your favourite number between 1 and 100: ";
     std::cin >> favourite_number;
+    
     std::cout << "Amazing!! That's my favourite number too!" << std::endl;
     std::cout << "No really!!, " << favourite_number << " is my favourite number!" << std::endl;
     
