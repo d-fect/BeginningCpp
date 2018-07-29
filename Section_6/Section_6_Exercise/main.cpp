3b267bd55e919b3c8b2b6ff0b7ac179fcd5be3cf
BFB10\frank <frank@d-fect.com>
2018-07-29 13:23:34 +0200

    Section 6: Updated with coding exercises

---
+++ Section_6/Section_6_Exercise/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;    // BAD PRACTICE! Don't use using!! But since I'm lazy I'll use it during this course.
+
+int main() {
+    // I will only include the code that are "between the lines"
+    // DO NOT LOOK HERE BEFORE YOU HAVE EXHAUSTED ALL OTHER POSSIBILITES
+    // At least look through the section videos once more
+    // THEN use Google
+    // THEN use the Q&A in the course
+    // THEN sleep on it
+    // Have you asked your friendly rubber duck zen master for help? https://rubberduckdebugging.com/
+    // OK, feel free to look at the code below
+    // I will not guarantee it's the best solution, but my solutions are tested on Udemy and passes
+    
+    // Exercise 4: 
+    int age {};
+    double hourly_wage {23.5};
+    string name {};
+    
+    cin >> name >> age;
+    
+    cout << endl;
+    return 0;
+}
