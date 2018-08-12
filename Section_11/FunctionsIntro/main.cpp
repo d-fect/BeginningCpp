9ed77233460066f29b11178fd8b92e4a88a7b435
BFB10\frank <frank@d-fect.com>
2018-08-12 23:07:41 +0200

    Section 11: Updated the program visualising the random bug

--- Section_11/FunctionsIntro/main.cpp
+++ Section_11/FunctionsIntro/main.cpp
@@ -1,7 +1,9 @@
+// Made a visual demo of the difference between to the random_number-statements above    
+// Filling a vector with number of times each number got randomly selected
+// Then displaying the contents of the vector, ie. how many times a number got selected
+
 #include <iostream>
 #include <ctime>
-//#include <cmath>
-//#include <cstdlib>
 #include <vector>
 
 using namespace std;    // BAD PRACTICE! Don't use using!! But since I'm lazy I'll use it during this course.
@@ -10,34 +12,36 @@ int main() {
     int random_number {};
     size_t count {100000};  // How many random numbers do you want to generate?
     int min {20};          // Must be <= max
-    int max {30};          // Must be >= min
+    int max {29};          // Must be >= min
+    vector <size_t> number_distribution (min + max + 1, 0);
+    size_t total_numbers {};
+    size_t unique_numbers {};
     
     cout << "RAND_MAX on my system is: " << RAND_MAX << endl;
     srand(time(nullptr));       // Seeding the random number generator to get different series of number each time program runs
     
-    // Made a visual demo of the difference between to the random_number-statements above    
-    // Filling a vector with number of times each number got randomly selected
-    vector <size_t> number_distribution (min + max + 1, 0);
-    size_t total {};
-    
+    // Run only ONE of the random_number-statements in this loop
     for(size_t i {1}; i <= count; i++) {
-//        random_number = rand() % (max - min + 1) + min;       // Correct version!!
-        random_number = rand() % max + min;     // NOT correct!!
+        random_number = rand() % (max - min + 1) + min;         // Correct version!!
+//        random_number = rand() % max + min;                     // NOT correct!!
         number_distribution.at(random_number)++;
     }
     
-    // Printing out a table of numers and times selected
+    // Printing out a table of numbers and how many times that got selected
     cout << "\nmin: \t" << min << endl;
     cout << "max: \t" << max << endl;
     cout << "count: \t" << count << endl;
     cout << "\nnumber\ttimes" << endl;
     cout << "=============" << endl;
     for(size_t i {0}; i < number_distribution.size(); i++) {
-        total += number_distribution.at(i);
-        if (number_distribution.at(i) !=0)
+        total_numbers += number_distribution.at(i);
+        if (number_distribution.at(i) !=0) {
             cout << i << "\t" << number_distribution.at(i) << endl;
+            unique_numbers++;
+        }
     }
-    cout << "\nA sum total of " << total << " numbers was randomly selected. Which " << (total == count ? "is " : "IS NOT ") << "the same as the variable count." << endl;
+    cout << "\nNumber of unique numbers selected: " << unique_numbers << endl;
+    cout << "Total numbers selected: " << total_numbers << ". This " << (total_numbers == count ? "is " : "IS NOT ") << "the same as the count variable." << endl;
     
     cout << endl;
     return 0;
