ad532e91361ffd62bb35b25b84d08e9cabee57fb
BFB10\frank <frank@d-fect.com>
2018-08-13 11:39:02 +0200

    Section 11: Update with input from instructor

--- Section_11/FunctionsIntro/main.cpp
+++ Section_11/FunctionsIntro/main.cpp
@@ -1,18 +1,15 @@
-// Made a visual demo of the difference between to the random_number-statements above    
-// Filling a vector with number of times each number got randomly selected
-// Then displaying the contents of the vector, ie. how many times a number got selected
-
 #include <iostream>
 #include <ctime>
 #include <vector>
-
+#include <iomanip>
+ 
 using namespace std;    // BAD PRACTICE! Don't use using!! But since I'm lazy I'll use it during this course.
-
+ 
 int main() {
     int random_number {};
-    size_t count {100000};  // How many random numbers do you want to generate?
-    int min {20};          // Must be <= max
-    int max {29};          // Must be >= min
+    size_t count {10000000};  // How many random numbers do you want to generate?
+    int min {1};          // Must be <= max
+    int max {20};          // Must be >= min
     vector <size_t> number_distribution (min + max + 1, 0);
     size_t total_numbers {};
     size_t unique_numbers {};
@@ -31,12 +28,13 @@ int main() {
     cout << "\nmin: \t" << min << endl;
     cout << "max: \t" << max << endl;
     cout << "count: \t" << count << endl;
-    cout << "\nnumber\ttimes" << endl;
-    cout << "=============" << endl;
+    cout << "\nnumber\ttimes\tpercent" << endl;
+    cout << "=======================" << endl;
+    cout << fixed << setprecision(2);
     for(size_t i {0}; i < number_distribution.size(); i++) {
         total_numbers += number_distribution.at(i);
         if (number_distribution.at(i) !=0) {
-            cout << i << "\t" << number_distribution.at(i) << endl;
+            cout << i << "\t" << number_distribution.at(i) << "\t" << setw(7) << right << static_cast<double>(number_distribution.at(i))/count * 100 << endl;
             unique_numbers++;
         }
     }
